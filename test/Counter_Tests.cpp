cb67001acb8b82f34477e9d40ce2e3e3f5b39a99
qegabbe <qegabbe@outlook.de>
2016-03-14 20:14:19 +0100

    Create Counter_Tests.cpp

---
+++ test/Counter_Tests.cpp
@@ -0,0 +1,19 @@
+/*
+ * Counter_Tests.cpp
+ *
+ *  Created on: Mar 14, 2016
+ *      Author:
+ */
+
+#include <gtest/gtest.h>
+
+#include "../src/Counter.hpp"
+
+TEST(Counter, Increment) {
+      Counter c;
+      EXPECT_EQ(0, c.Increment());
+      EXPECT_EQ(1, c.Increment());
+      EXPECT_EQ(2, c.Increment());
+}
+
+
