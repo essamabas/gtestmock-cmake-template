93b04f317b8182dc125b177128435f41b67947cd
qegabbe <qegabbe@outlook.de>
2016-03-14 20:13:40 +0100

    Create Counter.cpp

---
+++ src/Counter.cpp
@@ -0,0 +1,16 @@
+/*
+ * Counter.cpp
+ *
+ *  Created on: Mar 14, 2016
+ *      Author: 
+ */
+
+#include "Counter.hpp"
+
+#include <stdio.h>
+
+int Counter::Increment() {
+      return mCounter++;
+}
+
+
