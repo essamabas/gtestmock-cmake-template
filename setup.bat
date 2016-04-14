33ec4529a6c37d74fc4ab85cbf792a66ba3fb820
qegabbe <qegabbe@outlook.de>
2016-04-15 00:23:43 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,10 @@
 mkdir Debug
-cd Debug 
+cd Debug
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DGMOCK_ROOT=lib/googletest-release-1.7.0
+cd lib/googletest-release-1.7.0/make
 make clean
+make
+cd ..
+cd ..
+cd ..
 make
