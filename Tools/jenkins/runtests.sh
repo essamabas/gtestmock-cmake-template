41e0b5f03283f1319ececf8502b43c5faca98e00
qegabbe <qegabbe@outlook.de>
2016-06-02 02:39:43 -0400

    modify gcovr settings

--- Tools/jenkins/runtests.sh
+++ Tools/jenkins/runtests.sh
@@ -2,6 +2,7 @@
 
 cd ..
 cd ..
+#sudo chmod -R 777 debug/
 cd debug
 echo ">> run compiled application ..."
 if [ $# -eq 0 ]
@@ -14,11 +15,12 @@ if [ $# -eq 0 ]
 fi
 echo ">> generating coverage report ..."
 #python gcovr -x -r .. -f".*src\\.*" > coverage.xml2
-python gcovr -x -r .. -e".+\.test\.cpp" > coverage.xml
+#python gcovr -x -r .. -e".+\.test\.cpp" > coverage.xml
+python gcovr -x -r .. -e".test\*" > coverage.xml
 #
 echo ">> running cpp-check on src ..."
 #python gcovr -x -r .. -f".*src\\.*" > coverage.xml2
-sudo cppcheck --enable=all --inconclusive --xml-version=2 ../src 2> cppcheck.xml
+sudo cppcheck --enable=all --inconclusive --check-config --xml-version=2 ../src 2> cppcheck.xml
 echo ">> moving files to build-number Report .."
 #kdir ..\..\builds\%BUILD_NUMBER%\workspace-files
 #opy *.xml ..\..\builds\%BUILD_NUMBER%\workspace-files\*.xml
