7c10c6b3d72c76ae8e24ef3ff099466913853a84
qegabbe <qegabbe@outlook.de>
2016-06-02 05:06:11 -0400

    Try Html-Publish

--- src/Counter.hpp
+++ src/Counter.hpp
@@ -5,6 +5,19 @@
  *      Author: asma
  */
 
+/**
+ * @file Counter.hpp
+ * @author Me
+ * @date 9 Mar 2016
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ */
+
 #ifndef COUNTER_HPP_
 #define COUNTER_HPP_
 
